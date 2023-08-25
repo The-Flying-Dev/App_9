b3e1925298e375b3eabe4b483151966a090bf4d2
Leandran <41592532+Leandran@users.noreply.github.com>
2023-08-26 00:00:04 +0200

    Add FileReader class

--- WordUnscrambler/Workers/FileReader.cs
+++ WordUnscrambler/Workers/FileReader.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO; 
 
 namespace WordUnscrambler.Workers
 {
     class FileReader
     {
+        public string[] Read(string filename)
+        {
+            string[] fileContent;
+            try
+            {              // .IO directive above allows static methods to be called
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message); //wraps existing exception into this new exception, otherwise it eraises the entire call stack
+                // throw ex; DON'T DO THIS, ERAISES CALL STACK
+            }
+            return fileContent;
+        }
     }
 }
